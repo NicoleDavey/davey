fda9d1e9f3106b6f3808a10d0c00dd90bbd38553
Dave Strus <dave@getfretless.com>
2018-12-01 16:04:09 -0500

    Change a second heading when the button is clicked

--- catalog/README.md
+++ catalog/README.md
@@ -1,1 +1,9 @@
 # CATalog
+
+## Challenge 1
+
+Add a second heading to the page, and make the button change _that_ heading, instead of the first one. (Use a `class` or `id`.)
+
+### Challenge 1, part 2
+
+Make both headings change to different text.
--- catalog/index.js
+++ catalog/index.js
@@ -1,6 +1,9 @@
 function changeHeading() {
   let heading = document.querySelector('h1');
   heading.textContent = 'Ye Olde CATalog';
+
+  let heading2 = document.querySelector('.changeMe');
+  heading2.textContent = 'Oh yeah';
 }
 
 let button = document.querySelector('button');
