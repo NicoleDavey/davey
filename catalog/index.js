abf4ab58f1bcdebaf88d33f1abdb14bfbd688e04
Dave Strus <dave@getfretless.com>
2018-12-01 15:32:07 -0500

    Change heading when button is clicked

--- catalog/index.js
+++ catalog/index.js
@@ -1,1 +1,7 @@
-console.log('testing');
+function changeHeading() {
+  let heading = document.querySelector('h1');
+  heading.textContent = 'Ye Olde CATalog';
+}
+
+let button = document.querySelector('button');
+button.addEventListener('click', changeHeading);
