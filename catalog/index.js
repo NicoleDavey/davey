a065b347ea3af9ac5c9d9457ded5579b425ee534
Dave Strus <dave@getfretless.com>
2018-12-15 13:53:09 -0500

    Display each cat in its favorite color

--- catalog/index.js
+++ catalog/index.js
@@ -5,12 +5,13 @@ function changeHeading(ev) {
   // get the values from the form
   let catName = catForm.catName.value;
   let catAge = catForm.catAge.value;
+  let favoriteColor = catForm.favoriteColor.value;
 
   // grab the list from the page
   let catList = document.querySelector('ul.catList');
 
   // add the values to the list
-  catList.innerHTML += `<li>${catName}, ${catAge}</li>`;
+  catList.innerHTML += `<li style="color: ${favoriteColor}">${catName}, ${catAge}</li>`;
 
   // clear the form
   catForm.reset();
