6dd3c7b6c7a754a096984e3972105d2b02088562
Dave Strus <dave@getfretless.com>
2018-12-15 12:54:15 -0500

    Update two headings with form data

--- catalog/index.js
+++ catalog/index.js
@@ -1,9 +1,20 @@
-function changeHeading() {
-  let heading = document.querySelector('h1');
-  let heading2 = document.querySelector('.changeMe');
-  heading.textContent = 'Ye Olde CATalog';
-  heading2.textContent = 'Oh yeah';
+function changeHeading(ev) {
+  ev.preventDefault();
+  let catForm = ev.target;
+
+  // change the h1
+  let catName = catForm.catName.value;
+  let heading1 = document.querySelector('h1');
+  heading1.textContent = catName;
+
+  // change the h2
+  let catAge = catForm.catAge.value;
+  let ageHeading = document.querySelector('h2.age');
+  ageHeading.textContent = catAge;
+
+  // clear the form
+  catForm.reset();
 }
 
-let button = document.querySelector('button');
-button.addEventListener('click', changeHeading);
+let form = document.querySelector('#catForm');
+form.addEventListener('submit', changeHeading);
