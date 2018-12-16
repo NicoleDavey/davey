6e96cabb733f43e41c29344e9095d5516d8f6c46
Dave Strus <dave@getfretless.com>
2018-12-16 14:07:00 -0500

    Add weight

--- catalog/index.js
+++ catalog/index.js
@@ -7,13 +7,18 @@ function changeHeading(ev) {
     name: catForm.catName.value,
     age: catForm.catAge.value,
     favoriteColor: catForm.favoriteColor.value,
+    weight: catForm.weight.value,
   };
 
   // grab the list from the page
   let catList = document.querySelector('ul.catList');
 
   // add the values to the list
-  catList.innerHTML += `<li style="color: ${cat.favoriteColor}">${cat.name}, ${cat.age}</li>`;
+  catList.innerHTML += `
+    <li style="color: ${cat.favoriteColor}">
+      ${cat.name}, ${cat.age} (${cat.weight} lbs)
+    </li>
+  `;
 
   // clear the form
   catForm.reset();
